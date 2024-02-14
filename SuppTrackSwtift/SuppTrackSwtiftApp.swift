a274b2873061ceb7097b3885a491aaf1112cdffc
taylorlea43 <taylor.lea@quinnipiac.edu>
2024-02-14 17:51:19 -0500

    Initial Commit

---
+++ SuppTrackSwtift/SuppTrackSwtiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuppTrackSwtiftApp.swift
+//  SuppTrackSwtift
+//
+//  Created by Taylor Lea on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SuppTrackSwtiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
