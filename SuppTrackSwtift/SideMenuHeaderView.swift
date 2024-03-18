6e41b6ca4aa77afa1870ce2a11b5b0e0a1500e17
taylorlea43 <taylor.lea@quinnipiac.edu>
2024-03-18 18:29:14 -0400

    finishing details to create account and login. Also added side menu to the home screen.

---
+++ SuppTrackSwtift/SideMenuHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuHeaderView.swift
+//  SuppTrackSwtift
+//
+//  Created by Taylor Lea on 3/18/24.
+//
+
+import SwiftUI
+
+struct SideMenuHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuHeaderView()
+}
---
+++ SuppTrackSwtift/SideMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuView.swift
+//  SuppTrackSwtift
+//
+//  Created by Taylor Lea on 3/18/24.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuView()
+}
