9d97a6bb23904cbd64f53fbb99ec6c74ea00a32f
taylorlea43 <taylor.lea@quinnipiac.edu>
2024-03-20 18:14:58 -0400

    Hamburger menu now goes to home and is available on scanner and recently viewed. Also a user cannot go back to the login page after being on the home screen.

---
+++ SuppTrackSwtift/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  SuppTrackSwtift
+//
+//  Created by Taylor Lea on 3/20/24.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile()
+}
