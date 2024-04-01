d2cb2f41c6e682a3fedae26fd9adf8d48828b377
taylorlea43 <taylor.lea@quinnipiac.edu>
2024-04-01 11:21:10 -0400

    added hamburger menu and home screen

---
+++ SuppTrackSwtift/Welcome.swift
@@ -0,0 +1,18 @@
+//
+//  Welcome.swift
+//  SuppTrackSwtift
+//
+//  Created by Taylor Lea on 4/1/24.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Welcome()
+}
